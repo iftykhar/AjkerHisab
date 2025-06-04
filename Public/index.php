03cde27f432c6b42eae83e914ffb15f766bcd71a
iftykhar alam <s.m.ifty49@gmail.com>
2025-06-05 01:21:17 +0600

    login thgough the index page

--- Public/index.php
+++ Public/index.php
@@ -1,1 +1,14 @@
 <?php
+require_once '../App/Core/Router.php';
+require_once '../App/Core/Session.php';
+
+use App\Core\Router;
+use App\Core\Session;
+
+Session::start();
+
+
+$url = $_GET['url'] ?? '';
+$router = new Router();
+$router->route($url);
+
---
+++ Public/login.php
@@ -0,0 +1,36 @@
+<?php 
+use App\Core\Session;
+Session::start();
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $email = $_POST['email'];
+    $pass = $_POST['password'];
+
+    //testing purpose
+    if($email === 'admin@mail.com' && $pass === '123456'){
+        Session::set('user',$email);
+        header('location:/dashboard');
+        exit;
+    }else{
+        echo "Invalid Login";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AjkerHisab Login</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email">
+        <label for="Password">Password </label>
+        <input type="password" name="password" id="password">
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
