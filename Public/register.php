c5df2ebf3669969f46d3574d0cacf3f73604c064
iftykhar alam <s.m.ifty49@gmail.com>
2025-06-10 23:59:21 +0600

    register

--- Public/register.php
+++ Public/register.php
@@ -1,3 +1,9 @@
 <?php
-require_once '../App/config.php';
-require_once '../App/Core/Session.php';
+session_start();
+
+$usersFile = '../Storage/users.json';
+$errors = [];
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $name = trim($_POST['name'] ?? '');
+}
